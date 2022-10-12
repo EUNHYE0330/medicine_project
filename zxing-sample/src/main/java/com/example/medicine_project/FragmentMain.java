471bce1f482246cf59db60bebb33fb064dadf978
DongSik <75015314+Dong-Sik@users.noreply.github.com>
2022-10-12 16:06:18 +0900

    메인 프레그먼트 레이아웃 ( 팝업창 ) 임시 처리

--- zxing-sample/src/main/java/com/example/medicine_project/FragmentMain.java
+++ zxing-sample/src/main/java/com/example/medicine_project/FragmentMain.java
@@ -1,15 +1,22 @@
 package com.example.medicine_project;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 
@@ -21,9 +28,29 @@ import me.dm7.barcodescanner.zxing.sample.R;
 
 public class FragmentMain extends ListFragment  {
 
+    private PopupWindow popupWindow;
+    private LayoutInflater layoutInflater;
+    private FrameLayout frameLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+
+
+    @Override
+    public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        frameLayout = (FrameLayout) getView().findViewById(R.id.frameLayout);
+        // inflate the layout of the popup window
+        layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View popupView = layoutInflater.inflate(R.layout.popup_modify, null);
+        ViewGroup container =(ViewGroup) layoutInflater.inflate(R.layout.popup_modify, null);
+        popupWindow = new PopupWindow(container, 750, 250, true);
+        popupWindow.showAtLocation(frameLayout, Gravity.NO_GRAVITY, 180, 1350);
+
     }
 
     @Override
@@ -37,6 +64,7 @@ public class FragmentMain extends ListFragment  {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.single_row, R.id.rowTextView, userList);
         setListAdapter(adapter);
 
+        //l.setOnItemClickListener();
 
 
         //fragment에서 버튼 클릭으로 activity로 이동하기
