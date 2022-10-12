5e6deed251ec784f0fb04ec37546d4a0473c30bf
DongSik <75015314+Dong-Sik@users.noreply.github.com>
2022-10-12 15:27:51 +0900

    메인 프레그먼트 레이아웃 임시 처리

--- zxing-sample/src/main/java/com/example/medicine_project/FragmentMain.java
+++ zxing-sample/src/main/java/com/example/medicine_project/FragmentMain.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import me.dm7.barcodescanner.zxing.sample.R;
 
-public class FragmentMain extends Fragment {
+public class FragmentMain extends ListFragment  {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,13 @@ public class FragmentMain extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] userList = {"아네모정"};
+        ListView l = (ListView) view.findViewById(R.id.list_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.single_row, R.id.rowTextView, userList);
+        setListAdapter(adapter);
+
+
 
         //fragment에서 버튼 클릭으로 activity로 이동하기
         Button alarmButton = view.findViewById(R.id.bt_setAR);
